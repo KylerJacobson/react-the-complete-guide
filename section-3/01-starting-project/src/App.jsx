c1ea0b2574cba697c689e749aba2d59feb642b74
Kyler Jacobson <40446054+KylerJacobson@users.noreply.github.com>
2025-03-10 08:28:09 -0600

    implements section 3 up to lesson 50

--- section-3/01-starting-project/src/App.jsx
+++ section-3/01-starting-project/src/App.jsx
@@ -1,19 +1,34 @@
+import { CORE_CONCEPTS } from "./data";
+import Header from "./components/Header";
+import CoreConcept from "./components/CoreConcept";
+import TabButton from "./components/TabButton";
+
 function App() {
-  return (
-    <div>
-      <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
-      <main>
-        <h2>Time to get started!</h2>
-      </main>
-    </div>
-  );
+    return (
+        <div>
+            <Header />
+            <main>
+                <section id="core-concepts">
+                    <h2>Core Concepts</h2>
+                    <ul>
+                        <CoreConcept {...CORE_CONCEPTS[0]} />
+                        <CoreConcept {...CORE_CONCEPTS[1]} />
+                        <CoreConcept {...CORE_CONCEPTS[2]} />
+                        <CoreConcept {...CORE_CONCEPTS[3]} />
+                    </ul>
+                </section>
+                <section id="examples">
+                    <h2>examples</h2>
+                    <menu>
+                        <TabButton>Components</TabButton>
+                        <TabButton>JSX</TabButton>
+                        <TabButton>Props</TabButton>
+                        <TabButton>State</TabButton>
+                    </menu>
+                </section>
+            </main>
+        </div>
+    );
 }
 
 export default App;
