59e7c8e2c232755ac64a13cc499c5703bc569fcd
Kyler Jacobson <40446054+KylerJacobson@users.noreply.github.com>
2025-03-15 12:41:48 -0600

    finishes section 3

--- section-3/01-starting-project/src/App.jsx
+++ section-3/01-starting-project/src/App.jsx
@@ -1,9 +1,16 @@
-import { CORE_CONCEPTS } from "./data";
+import { useState } from "react";
+
+import { CORE_CONCEPTS, EXAMPLES } from "./data";
 import Header from "./components/Header";
 import CoreConcept from "./components/CoreConcept";
 import TabButton from "./components/TabButton";
 
 function App() {
+    const [selectedTopic, setSelectedTopic] = useState();
+    function handleSelect(selectedButton) {
+        console.log("Selected button:", selectedButton);
+        setSelectedTopic(selectedButton);
+    }
     return (
         <div>
             <Header />
@@ -11,20 +18,40 @@ function App() {
                 <section id="core-concepts">
                     <h2>Core Concepts</h2>
                     <ul>
-                        <CoreConcept {...CORE_CONCEPTS[0]} />
-                        <CoreConcept {...CORE_CONCEPTS[1]} />
-                        <CoreConcept {...CORE_CONCEPTS[2]} />
-                        <CoreConcept {...CORE_CONCEPTS[3]} />
+                        {CORE_CONCEPTS.map((concept) => (
+                            <CoreConcept {...concept} key={concept.title} />
+                        ))}
                     </ul>
                 </section>
                 <section id="examples">
                     <h2>examples</h2>
                     <menu>
-                        <TabButton>Components</TabButton>
-                        <TabButton>JSX</TabButton>
-                        <TabButton>Props</TabButton>
-                        <TabButton>State</TabButton>
+                        {CORE_CONCEPTS.map((concept) => (
+                            <TabButton
+                                isSelected={
+                                    selectedTopic ===
+                                    concept.title.toLowerCase()
+                                }
+                                onSelect={() =>
+                                    handleSelect(concept.title.toLowerCase())
+                                }
+                            >
+                                {concept.title}
+                            </TabButton>
+                        ))}
                     </menu>
+
+                    {!selectedTopic ? (
+                        <p>Please select a topic</p>
+                    ) : (
+                        <div id="tab-content">
+                            <h3>{EXAMPLES[selectedTopic].title}</h3>
+                            <p>{EXAMPLES[selectedTopic].description}</p>
+                            <pre>
+                                <code>{EXAMPLES[selectedTopic].code}</code>
+                            </pre>
+                        </div>
+                    )}
                 </section>
             </main>
         </div>
