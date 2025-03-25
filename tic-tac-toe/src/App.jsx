3a475c26d4f1653fa291f2d290841f7725a2f353
Kyler Jacobson <40446054+KylerJacobson@users.noreply.github.com>
2025-03-25 08:37:06 -0600

    implements up to lecture 83

--- tic-tac-toe/src/App.jsx
+++ tic-tac-toe/src/App.jsx
@@ -1,13 +1,31 @@
+import { useState } from "react";
 import Player from "./components/Player";
+import GameBoard from "./components/GameBoard";
 function App() {
+    const [activePlayer, setActivePlayer] = useState("X");
+    function handleSelectSquare() {
+        setActivePlayer((currentPlayer) => (currentPlayer === "X" ? "O" : "X"));
+        console.log("active player: ", activePlayer);
+    }
     return (
         <main>
             <div id="game-container">
-                <ol id="players">
-                    <Player name="Player 1" symbol="X" />
-                    <Player name="Player 2" symbol="O" />
+                <ol id="players" className="highlight-player">
+                    <Player
+                        name="Player 1"
+                        symbol="X"
+                        isActive={activePlayer === "X"}
+                    />
+                    <Player
+                        name="Player 2"
+                        symbol="O"
+                        isActive={activePlayer === "O"}
+                    />
                 </ol>
-                GAME BOARD
+                <GameBoard
+                    onSelectSquare={handleSelectSquare}
+                    activePlayer={activePlayer}
+                />
             </div>
             LOG
         </main>
--- tic-tac-toe/src/components/Player.jsx
+++ tic-tac-toe/src/components/Player.jsx
@@ -1,19 +1,28 @@
 import { useState } from "react";
 
-export default function Player({ name, symbol }) {
+export default function Player({ name, symbol, isActive }) {
+    const [playerName, setPlayerName] = useState(name);
     const [isEditing, setIsEditing] = useState(false);
 
     function handleClick() {
         setIsEditing((editing) => !editing);
     }
+    function handleChange(event) {
+        setPlayerName(event.target.value);
+    }
     return (
         <>
-            <li>
+            <li className={isActive ? "active" : undefined}>
                 <span className="player">
                     {isEditing ? (
-                        <input type="text" required value={name} />
+                        <input
+                            type="text"
+                            required
+                            onChange={handleChange}
+                            defaultValue={playerName}
+                        />
                     ) : (
-                        <span className="player-name">{name}</span>
+                        <span className="player-name">{playerName}</span>
                     )}
                     <span className="player-symbol">{symbol}</span>
                 </span>
